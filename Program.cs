6caf8d3d8894f067b1510d70870d3186fd52fdbc
Salvatore <dested@gmail.com>
2012-05-13 13:21:50 -0700

    better player based stress test

--- Program.cs
+++ Program.cs
@@ -11,19 +11,17 @@ namespace ShuffleSlam
 {
     class Program
     {
+        private static string[] names = new string[] { "joe", "mike", "ana", "sal", "jeff", "todd" };
         static void Main(string[] args)
         {
-            var numberOfGames = 100;
+            var numberOfGames = 200;
             var maxPlayers = 6;
             var rand = new Random();
-            ThreadPool.SetMaxThreads(500, 500);
-
 
             int gamesOpen = 0;
             for (int i = 0; i < numberOfGames; i++)
             {
-                var currentGame = new ShuffleGame();
-                var numOfPlayers = rand.Next(maxPlayers / 2,maxPlayers);
+                var numOfPlayers = rand.Next(maxPlayers / 2, maxPlayers);
                 for (int j = 0; j < numOfPlayers; j++)
                 {
                     /*ThreadPool.QueueUserWorkItem(Target, new
@@ -33,14 +31,21 @@ namespace ShuffleSlam
                                                              });
                     */
                     Thread t = new Thread(Target);
-                    t.Start(new
+                    t.Start(new ThreadParams()
                                 {
-                                    ind = j,
-                                    log = (Action<string>) (logs)
+                                    Seed = j * 6 + i * 20,
+                                    RoomIndex = i,
+                                    State = (j == 0 ? (ThreadState.CreateGame) : (j == numOfPlayers - 1 ? ThreadState.StartGame : ThreadState.JoinGame)),
+                                    UserName = names[j]
                                 });
+
                     gamesOpen++;
-//                     Console.ReadLine();
+                    Console.WriteLine(string.Format("{0} Games Opened", gamesOpen));
+                    //Console.ReadLine();
+                    Thread.Sleep(rand.Next(150, 450));
+
                 }
+                Thread.Sleep(rand.Next(400 , 550 ));
             }
 
             Console.WriteLine(string.Format("{0} Games Opened", gamesOpen));
@@ -50,87 +55,102 @@ namespace ShuffleSlam
 
         private static void Target(object parameters)
         {
-            Action<string> log = (Action<string>)parameters.GetType().GetProperty("log").GetValue(parameters,null);
-            Random rand = new Random((int)parameters.GetType().GetProperty("ind").GetValue(parameters,null));
+            var options = (ThreadParams)parameters;
+            Random rand = new Random(options.Seed);
 
-            var id = Guid.NewGuid().ToString();
             var socket = new Client("http://50.116.22.241:81/"); // url to nodejs 
             socket.Opened += SocketOpened;
             socket.Message += SocketMessage;
             socket.SocketConnectionClosed += SocketConnectionClosed;
             socket.Error += SocketError;
 
+            string roomID = null;
             // register for 'connect' event with io server
-            socket.On("Area.Game.AskQuestion.sal", (fn) =>
-            {
-//                Console.WriteLine(fn.MessageText);
-                log(id + " stta");
-                socket.Emit("Area.Game.AnswerQuestion", new { value = 1 });
-            
-   //  Thread.Sleep(rand.Next(400,2400));
 
-            });
 
             socket.On("Area.Game.AskQuestion", (fn) =>
             {
-                //              Console.WriteLine(fn.MessageText);
-                log(id + " answered");
-        //        socket.Emit("Area.Game.AnswerQuestion", new { value = 1 });
-         //       Thread.Sleep(rand.Next(400, 2400));
+                Thread.Sleep(rand.Next(400, 2000));
+                Console.WriteLine("asked: " + options.RoomIndex + "  " + options.UserName);
+                socket.Emit("Area.Game.AnswerQuestion", new { answer = 1, roomID = options.RoomIndex });
+            });
+
+
+            socket.On("Area.Game.UpdateState", (fn) =>
+            {
 
             });
+
+            socket.On("Area.Game.RoomInfo", (fn) =>
+                                                {
+                                                    roomID = fn.Json.Args[0].roomID.ToString();
+
+                                                });
 
             socket.On("Area.Game.GameOver", (fn) =>
             {
-                //              Console.WriteLine(fn.MessageText);
-                log(id + " Over");
+                socket.Close();
+                socket = null;
 
             });
-            socket.On("Area.Game.StartGame", (fn) =>
+            socket.On("Area.Game.Started", (fn) =>
             {
-    //            Console.WriteLine(fn.MessageText);
-                log(id + " started");
-                socket.Emit("Area.Game.AnswerQuestion", new { value = 1 });
 
             });
 
             // make the socket.io connection
             socket.Connect();
-            socket.Emit("Area.Game.StartGame",new {user="dested"});
-            log(id + " connected");
-            
-        }
+            switch (options.State)
+            {
+                case ThreadState.StartGame:
+                    socket.Emit("Area.Game.Join", new { user = new { name = options.UserName }, roomID = options.RoomIndex });
+                    socket.Emit("Area.Game.Start", new { user = new { name = options.UserName }, roomID = options.RoomIndex });
+                    break;
+                case ThreadState.JoinGame:
+                    socket.Emit("Area.Game.Join", new { user = new { name = options.UserName }, roomID = options.RoomIndex });
+                    break;
+                case ThreadState.CreateGame:
+                    socket.Emit("Area.Game.Create", new { user = new { name = options.UserName } });
 
-        private static void logs(string j)
-        {
-            
-            Console.WriteLine(size+"  "+j);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         private static void SocketError(object sender, ErrorEventArgs e)
         {
-            
+
         }
 
         private static void SocketConnectionClosed(object sender, EventArgs e)
         {
-            
+
 
         }
-[ThreadStatic]
 
-        private static long size = 0;
         private static void SocketMessage(object sender, MessageEventArgs e)
         {
-            size += e.Message.Json.ToJsonString().Length;
 
         }
 
         private static void SocketOpened(object sender, EventArgs e)
         {
-            
+
 
         }
+    }
+    public class ThreadParams
+    {
+        public int Seed { get; set; }
+        public ThreadState State { get; set; }
+        public string UserName { get; set; }
+
+        public int RoomIndex { get; set; }
+    }
+    public enum ThreadState
+    {
+        StartGame, JoinGame, CreateGame
     }
 
     internal class ShuffleGame
